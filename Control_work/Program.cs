c06022d46f6603bbdfeb3f335d0b786cee404c75
Сергей Валерьевич Матвеев <110776836+1sergei2@users.noreply.github.com>
2022-10-01 08:11:25 +0500

    Добавлен метод сокращения второго массива до 3-х элементов

--- Control_work/Program.cs
+++ Control_work/Program.cs
@@ -1,2 +1,14 @@
 ﻿string[] array = new string[5] {"123", "456", "789", "hello", "world"};
 string[] array1 = new string[array.Length];
+void SecondArrayWithIF(string[] array, string[] array1)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array1[count] = array1[i];
+        count++;
+        }
+    }
+}
