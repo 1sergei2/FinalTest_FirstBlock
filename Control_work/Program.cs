f147f40acabefc560b6d0169c1e8482e06bbf0fa
Сергей Валерьевич Матвеев <110776836+1sergei2@users.noreply.github.com>
2022-10-01 08:07:01 +0500

    Заданы массивы строк

--- Control_work/Program.cs
+++ Control_work/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[5] {"123", "456", "789", "hello", "world"};
+string[] array1 = new string[array.Length];
